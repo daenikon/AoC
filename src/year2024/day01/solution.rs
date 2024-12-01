996b4d86835e3435a709b5ee46925567932404c4
daenikon <dkondratiuk06@gmail.com>
2024-12-01 14:59:01 +0100

    add 01/2024 second part

--- src/year2024/day01/solution.rs
+++ src/year2024/day01/solution.rs
@@ -1,5 +1,6 @@
-pub fn run(input: &str) -> i32 {
+use std::collections::HashMap;
 
+pub fn run(input: &str) -> u32 {
     let mut vec1 = Vec::new();
     let mut vec2 = Vec::new();
 
@@ -8,26 +9,52 @@ pub fn run(input: &str) -> i32 {
             continue;
         }
 
-        let num1 = &line[0..5].parse::<i32>().unwrap();
-        let num2 = &line[8..13].parse::<i32>().unwrap();
+        let num1 = &line[0..5].parse::<u32>().unwrap();
+        let num2 = &line[8..13].parse::<u32>().unwrap();
         vec1.push(*num1);
         vec2.push(*num2);
         //println!("{} and {}", vec1[0], vec2[0]);
     }
 
-    println!("{}", vec1.len());
+    //println!("{}", vec1.len());
     vec1.sort();
     vec2.sort();
 
+    let total_distance: u32 = calculate_total_distance(&vec1, &vec2);
+    println!("Total distance: {}", total_distance);
 
-    let mut sum_of_diffs: i32 = 0;
+    let similarity_score: u32 = calculate_similarity_score(&vec1, &vec2);
+    println!("Similarity score: {}", similarity_score);
 
-    for i in 0..1000 {
-        let diff = (vec1[i] - vec2[i]).abs();
-        //println!("Num1: {} | Num2: {} | Diff: {} | i: {}", vec1[i], vec2[i], diff, i);
-        sum_of_diffs += diff;
+    return total_distance as u32;
+}
 
-        //println!("Num1: {}, Num2: {}, diff: {}", vec1[i], vec2[i], diff);
+// Input: sorted vectors
+fn calculate_total_distance(vec1: &Vec<u32>, vec2: &Vec<u32>) -> u32 {
+    let mut total_distance: u32 = 0;
+
+    for i in 0..vec1.len() {
+        let distance = vec1[i].abs_diff(vec2[i]);
+        total_distance += distance;
     }
-    return sum_of_diffs as i32;
+
+    total_distance
+}
+
+fn calculate_similarity_score(vec1: &Vec<u32>, vec2: &Vec<u32>) -> u32 {
+    let mut occurences = HashMap::new();
+    for i in 0..vec2.len() {
+        let temp = occurences.get(&vec2[i]).copied().unwrap_or(0);
+        occurences.insert(vec2[i], temp + 1);
+    }
+
+    let mut similarity_score: u32 = 0;
+    for i in 0..vec1.len() {
+        if (i < vec1.len() - 2 && vec1[i] == vec1[i+1]) {
+            continue;
+        }
+        let temp = occurences.get(&vec1[i]).copied().unwrap_or(0);
+        similarity_score += temp * vec1[i];
+    }
+    similarity_score
 }

c6ba4992fc6e6db06e8b0ba1349f62e0657f18ed
daenikon <dkondratiuk06@gmail.com>
2024-12-03 21:21:20 +0100

    add 2024/03 part 1 solution

--- src/year2024/day03/solution.rs
+++ src/year2024/day03/solution.rs
@@ -1,14 +1,18 @@
 use regex::Regex;
 
-pub fn run(input: &str) -> u16 {
-    let pattern = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+pub fn run(hay: &str) -> u32 {
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    //let mat = re.find(hay).unwrap();
+    //println!("Result: {}", mat.as_str());
+    //let caps = re.captures(hay).unwrap();
+    //println!("{}", caps.get(2).unwrap().as_str());
 
-    if let Some(caps) = pattern.captures(input) {
-        println!("Full match: {}", &caps[0]);
-        println!("First number: {}", &caps[1]);
-        println!("Second number: {}", &caps[2]);
-    } else {
-        println!("No match found!");
+    let mut sum: u32 = 0;
+    for (_, [mat1, mat2]) in re.captures_iter(hay).map(|c| c.extract()) {
+        let num1: u32 = mat1.parse::<u32>().unwrap();
+        let num2: u32 = mat2.parse::<u32>().unwrap();
+        println!("Num1: {}, Num2: {}", num1, num2);
+        sum += num1 * num2;
     }
-    0
+    sum
 }
